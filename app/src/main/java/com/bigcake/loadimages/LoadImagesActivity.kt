a57ea8ede3e9e52556423dcd585bf0de9a5cd74e
BigCake <huynhminhkiet@gmail.com>
2018-03-22 23:12:53 +0700

    add load image module

---
+++ app/src/main/java/com/bigcake/loadimages/LoadImagesActivity.kt
@@ -0,0 +1,15 @@
+package com.bigcake.loadimages
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.bigcake.R
+
+class LoadImagesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_load_images)
+
+        
+    }
+}
