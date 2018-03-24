2c060bd65ed9571cd2065b307bcae29de2275338
BigCake <huynhminhkiet@gmail.com>
2018-03-24 15:54:53 +0700

    add fuck layout

---
+++ app/src/main/java/com/bigcake/multiscreen/FuckLayoutActivity.kt
@@ -0,0 +1,13 @@
+package com.bigcake.multiscreen
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.bigcake.R
+
+class FuckLayoutActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_fuck_layout)
+  }
+}
